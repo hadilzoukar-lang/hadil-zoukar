03343d0f370bc800a5d48867b1567fd3274bb6b6
hadil zoukar <hadilzoukar@gmail.com>
2026-09-25 17:54:59 +0200

    mini-domotique
    Un mini système domotique avec Raspberry Pi. Permettant de surveiller l'environnement d'une habitation et de contrôler automatiquement différents équipements.

---
+++ main.py
@@ -0,0 +1,29 @@
+mini-domotique/
+│
+├── main.py
+│
+├── sensors/
+│   ├── __init__.py
+│   ├── dht22.py
+│   └── pir.py
+│
+├── actuators/
+│   ├── __init__.py
+│   ├── relay.py
+│   └── buzzer.py
+│
+├── automation/
+│   ├── __init__.py
+│   └── rules.py
+│
+├── database/
+│   ├── __init__.py
+│   └── database.py
+│
+├── web/
+│   ├── __init__.py
+│   ├── app.py
+│   └── templates/
+│       └── index.html
+│
+└── README.md
